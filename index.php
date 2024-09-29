25e9c0c62788af26a2e739c8420cf5e2000eb205
axe1a <150570262+axe1a@users.noreply.github.com>
2024-09-30 00:10:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php session_start(); ?>      
+    <form action="handleForm.php" method="POST">
+        <p>Username <input type="text" placeholder="First name here" name="firstName"></p>
+        <p>Password <input type="password" placeholder="Password here" name="password"></p>
+        <p><input type="submit" value="Login" name="submitBtn"></p>
+    </form>
+    <a href="unset.php"><input type="submit" value="Logout" name="logout"></p></a>
+    
+    <?php
+    if (isset($_SESSION['loginWarning'])) {
+        echo $_SESSION['loginWarning'];    
+    }
+    ?>
+    <h2>
+        
+        <?php
+        if(isset($_SESSION['firstName'])) {
+            echo 'User logged in: ';
+            echo $_SESSION['firstName'];
+        }
+        ?>      
+    </h2>
+
+    <h2>
+        <?php
+        if(isset($_SESSION['password'])) {
+            echo 'Password: ';
+            echo $_SESSION['password'];
+        }
+        ?>      
+    </h2>
+
+
+
+</body>
+</html>
---
+++ unset.php
@@ -0,0 +1,6 @@
+<?php  
+session_start(); // Establish connection to the current session
+session_unset(); // Delete all session variables
+header('Location: index.php'); // Go back to homepage
+
+?>
